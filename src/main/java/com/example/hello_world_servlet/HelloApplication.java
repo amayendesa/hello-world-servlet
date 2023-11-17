418e886730c2cfcea333d1a71e933ec410285955
Aaron Mayendesa <aaron.mayendesa@fbc.co.zw>
2023-11-17 13:09:36 +0200

    new changes

---
+++ src/main/java/com/example/hello_world_servlet/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.hello_world_servlet;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
